ec7d1ec174da57e52d74910e13fa2e8f07ea9e86
ns4ever <45993071+ns4ever@users.noreply.github.com>
2018-12-19 18:20:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 provider "google" {
- 
+ credentials =  "${file("https://demostoragecloud.blob.core.windows.net/democontainer/irm-fisma-gov-prod-844d35057239.json")}"
   project     = "iron-mountain-219809"
   region      = "us-east4"
 }
