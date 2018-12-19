5f05493732a7ef342a1b199184774172842d97a9
ns4ever <45993071+ns4ever@users.noreply.github.com>
2018-12-19 16:01:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,10 @@
-provider "google" {
-  credentials = "${file("https://github.com/ns4ever/terraform/blob/master/iron-mountain-219809-2306b39c7557.json")}"
-  project     = "${var.project}"
-  region      = "${var.region}"
-}
 
 
 
 resource "google_compute_network" "vpc" {
- name                    = "${var.vpcname}"
+ name                    = "testabc"
  auto_create_subnetworks = "false"
-routing_mode = "${var.routing_mode}"
+routing_mode = "GLOBAL"
 }
 
 
